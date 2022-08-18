93c5313abdac5a64f38eca01e3e22d95229c9c8b
Sowwybaka <90488562+Sowwybaka@users.noreply.github.com>
2022-08-17 19:28:57 -0700

    added copy image address button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,37 @@
 import React, { useState } from 'react'
 
 function Home() {
-    const [imgUrl, setimgUrl] = useState("welcome")
-    function getImage() {
-        const url = `https://source.unsplash.com/random/900×700/?${imgUrl}`
+    const [keyword, setkeyword] = useState("welcome")
+    const [imageUrl, setimageUrl] = useState("")
+    function copyImgAddress() {
+        setimageUrl(`https://source.unsplash.com/random/500×500/?${keyword}`)
+        navigator.clipboard.writeText(imageUrl)
     }
     function handleOnChange(event) {
-        setimgUrl(event.target.value)
+        setkeyword(event.target.value)
 
     }
-    getImage()
+
+
     return (
         <>
             <div className="container text-center">
                 <div className="row my-3">
                     <div className="col-md-6 m-auto ">
-                        <input value={imgUrl} className='w-75 rounded border-2 p-3 ' onChange={handleOnChange} placeholder='Search for Image' type="text" />
+                        <input value={keyword} className='w-75 rounded border-2 p-3 ' onChange={handleOnChange} placeholder='Search for Image' type="text" />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6 m-auto ">
-                        <img className='w-100 rounded' src={`https://source.unsplash.com/random/900×700/?${imgUrl}`} alt="image" />
+                        <img height="500px" className='bg-dark p-3 rounded' src={`https://source.unsplash.com/random/500×500/?${keyword}`} alt="image" />
+                    </div >
+                </div >
+                <div className="row">
+                    <div className="col">
+                        <button className="btn-danger btn my-2" onClick={copyImgAddress}>Copy Image Address</button>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
