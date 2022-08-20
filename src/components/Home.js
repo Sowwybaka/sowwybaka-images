6a3371208b57e463852c97bae3610323a046f3fc
Sowwybaka <90488562+Sowwybaka@users.noreply.github.com>
2022-08-19 19:22:34 -0700

    added loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 
 function Home() {
-    const [keyword, setkeyword] = useState("welcome")
-    const [imageUrl, setimageUrl] = useState("")
+    const [keyword, setkeyword] = useState("")
+    const [imageUrl, setimageUrl] = useState("https://cdn-icons-png.flaticon.com/512/14/14562.png")
     function copyImgAddress() {
         navigator.clipboard.writeText(imageUrl)
     }
@@ -13,6 +13,7 @@ function Home() {
     }
 
     async function fetchApi() {
+        setimageUrl("https://cdn.dribbble.com/users/29051/screenshots/2347771/spinner.mov.gif")
         let data = await fetch(`https://source.unsplash.com/random/500×500/?${keyword}`)
         setimageUrl(data["url"])
     }
@@ -23,7 +24,7 @@ function Home() {
             <div className="container text-center">
                 <div className="row my-3">
                     <div className="col-md-6 m-auto ">
-                        <input value={keyword} className='w-75 rounded border-2 p-3 ' onChange={handleOnChange} placeholder='Search for Image' type="text" />
+                        <input value={keyword} className='w-75 rounded border-2 p-3 ' onChange={handleOnChange} placeholder='enter keyword' type="text" />
                     </div>
                 </div>
                 <div className="row">
